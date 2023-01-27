2435fe26a756e6cb0a6cf83fc810320a57366c64
AngeloDamante <angelo.damante16@gmail.com>
2023-01-27 18:03:19 +0100

    test: add test for create dataset functions

---
+++ test/test_train_cnn.py
@@ -0,0 +1,23 @@
+import unittest
+import logging
+from src.create_dataset import load_dataset, prepare_dataset
+from src.init import configure_logging
+
+
+class TestTrainCNN(unittest.TestCase):
+    def test_load_dataset(self):
+        configure_logging("log_ut_create_dataset.log", True, log_lvl=logging.DEBUG)
+        flag, _, _ = load_dataset("../dataset")
+        self.assertEqual(flag, True)
+
+        flag, _, _ = load_dataset("/dataset")
+        self.assertEqual(flag, False)
+
+    def test_prepare_dataset(self):
+        _, a, b = load_dataset("../dataset")
+        flag, _ = prepare_dataset(a, b)
+        self.assertEqual(flag, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
